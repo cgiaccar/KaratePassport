6d4c38a3d9234157eee04a51ccb698f9936c9bbd
Camilla Giaccari <cami.lla@me.com>
2022-11-28 18:29:05 +0100

    It now writes the read data into the Logcat

--- app/src/main/java/it/units/karatepassport/MainActivity.java
+++ app/src/main/java/it/units/karatepassport/MainActivity.java
@@ -3,9 +3,13 @@ package it.units.karatepassport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +23,23 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("Message"); // w/ no parameter gets root reference
 
         myRef.setValue("Hello, World!");
+
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("VALUE","The value is: " + value);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("ERROR","Failed to read value", error.toException());
+            }
+        });
     }
+
 }
