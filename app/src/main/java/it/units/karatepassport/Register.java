34ca6ad0e2cafc4eb09f3ae4fda81472f8f94fbc
Camilla Giaccari <cami.lla@me.com>
2022-12-08 17:08:52 +0100

    Manifest has now INTERNET Permission and Register activity as launcher; also Register activity WORKS

--- app/src/main/java/it/units/karatepassport/Register.java
+++ app/src/main/java/it/units/karatepassport/Register.java
@@ -1,14 +1,90 @@
 package it.units.karatepassport;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+
+    EditText mEmail, mUsername, mPassportNumber, mPassword;
+    Button mRegisterBtn;
+    TextView mLoginHereBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mEmail = findViewById(R.id.email);
+        mUsername = findViewById(R.id.username);
+        mPassportNumber = findViewById(R.id.passportNumber);
+        mPassword = findViewById(R.id.password);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginHereBtn = findViewById(R.id.textLoginHere);
+        progressBar = findViewById(R.id.progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        // check if user is already logged in
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        // on button click
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                // check if user inputs are valid
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password must be at least six characters long");
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                // register the new user in Firebase
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
     }
 }
