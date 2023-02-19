ab780892d7fb82f113f674da93b8339a222c0832
Camilla Giaccari <cami.lla@me.com>
2023-02-19 20:58:55 +0100

    The user info are stored in DB at registration (issue #6)

--- app/src/main/java/it/units/karatepassport/Register.java
+++ app/src/main/java/it/units/karatepassport/Register.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,17 +15,27 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
+    public static final String TAG = "TAG";
     EditText mEmail, mUsername, mPassportNumber, mPassword;
     Button mRegisterBtn;
     TextView mLoginHereBtn;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,7 @@ public class Register extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         // check if user is already logged in
         if (fAuth.getCurrentUser() != null) {
@@ -53,6 +65,8 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPassword.getText().toString().trim();
+                String userName = mUsername.getText().toString();
+                String passportNumber = mPassportNumber.getText().toString();
 
                 // check if user inputs are valid
                 if (TextUtils.isEmpty(email)) {
@@ -77,6 +91,24 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             Toast.makeText(Register.this, "User created successfully!", Toast.LENGTH_SHORT).show();
+                            // as soon as the user is created, we save the user data into the Firebase Firestore Database
+                            userID = fAuth.getCurrentUser().getUid(); // get current user (currently registering user) unique ID
+                            DocumentReference documentReference = fStore.collection("users").document(userID); // creates a new user inside the collection of users using their unique ID
+                            Map<String,Object> user = new HashMap<>(); // the most popular method to create new data is by using an hashmap
+                            user.put("userName", userName);
+                            user.put("passportNumber", passportNumber);
+                            user.put("email", email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "onSuccess: user profile is created for " + userID); // log the success message in our logcat
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {   // for debug
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: " + e.toString());
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         } else {
                             Toast.makeText(Register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
