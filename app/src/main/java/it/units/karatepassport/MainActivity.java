bf4ae9914dc7535cb09f81b7745e0560aa1a6df6
Camilla Giaccari <cami.lla@me.com>
2023-02-20 17:20:25 +0100

    The main activity reads from online DB (issue #7)

--- app/src/main/java/it/units/karatepassport/MainActivity.java
+++ app/src/main/java/it/units/karatepassport/MainActivity.java
@@ -1,11 +1,13 @@
 package it.units.karatepassport;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -13,13 +15,44 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name,email,number;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        number = findViewById(R.id.mainPassportNumber);
+        name = findViewById(R.id.mainUserName);
+        email = findViewById(R.id.mainEmail);
+
+        // initialization of Auth e Firestore
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        // this is the reference to what we want to retrieve, to the specific user
+        DocumentReference documentReference = fStore.collection("users").document(userID);
+
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                //using the names assigned in Register.java, visible also in the FirestoreDB
+                number.setText(documentSnapshot.getString("passportNumber"));
+                name.setText(documentSnapshot.getString("userName"));
+                email.setText(documentSnapshot.getString("email"));
+            }
+        });
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
