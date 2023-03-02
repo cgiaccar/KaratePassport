147afd0efd6772277ee7bf6c6714a37af53a74f7
Camilla Giaccari <cami.lla@me.com>
2023-03-02 15:18:21 +0100

    Now the navigation has a working logout (#11); also fixed nullPointerException for snapshotListener in main at logout

--- app/src/main/java/it/units/karatepassport/MainActivity.java
+++ app/src/main/java/it/units/karatepassport/MainActivity.java
@@ -2,6 +2,7 @@ package it.units.karatepassport;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -43,16 +44,21 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(menuItem -> {
-            switch (menuItem.getItemId()) {
-                case R.id.nav_home:
-                    Toast.makeText(MainActivity.this, "The home is clicked!", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.nav_belts_log:
-                    return true;
-                case R.id.nav_grant_belt:
-                    return false;
-            }
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            Toast.makeText(MainActivity.this, "The home is clicked!", Toast.LENGTH_SHORT).show();
+                            return true;
+                        case R.id.nav_belts_log:
+                            return true;
+                        case R.id.nav_grant_belt:
+                            return false;
+                    }
             return true;
+        });
+        navigationView.getMenu().findItem(R.id.logout).setOnMenuItemClickListener(menuItem -> {
+            Toast.makeText(this, "Logging out", Toast.LENGTH_SHORT).show();
+            logout(findViewById(android.R.id.content).getRootView());
+            return false;
         });
 
 
@@ -63,16 +69,19 @@ public class MainActivity extends AppCompatActivity {
         DocumentReference documentReference = fStore.collection("users").document(userID);
 
         documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
-            //using the names assigned in Register.java, visible also in the FirestoreDB
-            String nameString = documentSnapshot.getString("userName");
-            number.setText(documentSnapshot.getString("passportNumber"));
-            name.setText(nameString);
-            email.setText(documentSnapshot.getString("email"));
+            if ((e == null ) || (documentSnapshot !=null && documentSnapshot.exists())) {
 
-            //change the "Logged in as ..." message in navigation header
-            headerView = navigationView.getHeaderView(0);
-            loggedAs = headerView.findViewById(R.id.loggedAs);
-            loggedAs.setText(getString(R.string.logged_as, nameString));
+                //using the names assigned in Register.java, visible also in the FirestoreDB
+                String nameString = documentSnapshot.getString("userName");
+                number.setText(documentSnapshot.getString("passportNumber"));
+                name.setText(nameString);
+                email.setText(documentSnapshot.getString("email"));
+
+                //change the "Logged in as ..." message in navigation header
+                headerView = navigationView.getHeaderView(0);
+                loggedAs = headerView.findViewById(R.id.loggedAs);
+                loggedAs.setText(getString(R.string.logged_as, nameString));
+            }
         });
     }
 
