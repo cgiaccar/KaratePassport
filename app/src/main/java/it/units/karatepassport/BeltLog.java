56016c2d4be5ac1135e3c3c9ffd761bb4ed57e5a
Camilla Giaccari <cami.lla@me.com>
2023-03-08 20:42:51 +0100

    BeltLog and GrantBelt are now (still empty) fragments (#15)

--- app/src/main/java/it/units/karatepassport/BeltLog.java
+++ app/src/main/java/it/units/karatepassport/BeltLog.java
@@ -1,14 +1,21 @@
 package it.units.karatepassport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class BeltLog extends AppCompatActivity {
+public class BeltLog extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_belt_log);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_belt_log, container, false);
+        return view;
     }
 }
