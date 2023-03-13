3eeb305cf27ae2fb5851aedca22b1af4cd6d38e9
Camilla Giaccari <cami.lla@me.com>
2023-03-13 18:34:52 +0100

    Method getNonNullDate in BeltLogFragment (#17)

--- app/src/main/java/it/units/karatepassport/BeltLogFragment.java
+++ app/src/main/java/it/units/karatepassport/BeltLogFragment.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import javax.annotation.Nullable;
@@ -20,7 +21,7 @@ import java.util.Date;
 
 public class BeltLogFragment extends Fragment {
 
-    TextView beltHere;
+    TextView whiteBeltDate, fifthDanDate;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
@@ -34,7 +35,8 @@ public class BeltLogFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        beltHere = getView().findViewById(R.id.white_belt_date);
+        whiteBeltDate = getView().findViewById(R.id.white_belt_date);
+        fifthDanDate = getView().findViewById(R.id.fifth_dan_date);
 
         // initialization of Auth e Firestore
         fAuth = FirebaseAuth.getInstance();
@@ -48,10 +50,18 @@ public class BeltLogFragment extends Fragment {
 
         documentReference.addSnapshotListener((documentSnapshot, e) -> {
             if ((e == null) || (documentSnapshot != null && documentSnapshot.exists())) {
-                Date timestamp = documentSnapshot.getTimestamp("white").toDate();
-                String date = DateFormat.format("MMMM dd, yyyy", timestamp).toString();
-                beltHere.setText(date);
+                whiteBeltDate.setText(getNonNullDate(documentSnapshot, "white"));
+                fifthDanDate.setText(getNonNullDate(documentSnapshot, "fifth"));
             }
         });
+    }
+
+    private String getNonNullDate(DocumentSnapshot documentSnapshot, String rank){
+        try {
+            Date timestamp = documentSnapshot.getTimestamp(rank).toDate();
+            return DateFormat.format("MMMM dd, yyyy", timestamp).toString();
+        } catch(NullPointerException e){
+            return "";
+        }
     }
 }
