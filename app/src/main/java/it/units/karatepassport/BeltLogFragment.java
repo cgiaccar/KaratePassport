5c8dce7d150bba2178c59880cdb37e439bb0d740
Camilla Giaccari <cami.lla@me.com>
2023-03-13 18:48:41 +0100

    BeltLogFragment now takes and overwrites all ranks (#17)

--- app/src/main/java/it/units/karatepassport/BeltLogFragment.java
+++ app/src/main/java/it/units/karatepassport/BeltLogFragment.java
@@ -21,7 +21,9 @@ import java.util.Date;
 
 public class BeltLogFragment extends Fragment {
 
-    TextView whiteBeltDate, fifthDanDate;
+    TextView whiteBeltDate, yellowBeltDate, orangeBeltDate, greenBeltDate, blueBeltDate,
+            brownBeltDate, firstDanDate, secondDanDate, thirdDanDate, fourthDanDate, fifthDanDate,
+            sixthDanDate, seventhDanDate, eighthDanDate, ninthDanDate, tenthDanDate;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
@@ -36,7 +38,21 @@ public class BeltLogFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         whiteBeltDate = getView().findViewById(R.id.white_belt_date);
+        yellowBeltDate = getView().findViewById(R.id.yellow_belt_date);
+        orangeBeltDate = getView().findViewById(R.id.orange_belt_date);
+        greenBeltDate = getView().findViewById(R.id.green_belt_date);
+        blueBeltDate = getView().findViewById(R.id.blue_belt_date);
+        brownBeltDate = getView().findViewById(R.id.brown_belt_date);
+        firstDanDate = getView().findViewById(R.id.first_dan_date);
+        secondDanDate = getView().findViewById(R.id.second_dan_date);
+        thirdDanDate = getView().findViewById(R.id.third_dan_date);
+        fourthDanDate = getView().findViewById(R.id.fourth_dan_date);
         fifthDanDate = getView().findViewById(R.id.fifth_dan_date);
+        sixthDanDate = getView().findViewById(R.id.sixth_dan_date);
+        seventhDanDate = getView().findViewById(R.id.seventh_dan_date);
+        eighthDanDate = getView().findViewById(R.id.eighth_dan_date);
+        ninthDanDate = getView().findViewById(R.id.ninth_dan_date);
+        tenthDanDate = getView().findViewById(R.id.tenth_dan_date);
 
         // initialization of Auth e Firestore
         fAuth = FirebaseAuth.getInstance();
@@ -51,7 +67,21 @@ public class BeltLogFragment extends Fragment {
         documentReference.addSnapshotListener((documentSnapshot, e) -> {
             if ((e == null) || (documentSnapshot != null && documentSnapshot.exists())) {
                 whiteBeltDate.setText(getNonNullDate(documentSnapshot, "white"));
+                yellowBeltDate.setText(getNonNullDate(documentSnapshot, "yellow"));
+                orangeBeltDate.setText(getNonNullDate(documentSnapshot, "orange"));
+                greenBeltDate.setText(getNonNullDate(documentSnapshot, "green"));
+                blueBeltDate.setText(getNonNullDate(documentSnapshot, "blue"));
+                brownBeltDate.setText(getNonNullDate(documentSnapshot, "brown"));
+                firstDanDate.setText(getNonNullDate(documentSnapshot, "first"));
+                secondDanDate.setText(getNonNullDate(documentSnapshot, "second"));
+                thirdDanDate.setText(getNonNullDate(documentSnapshot, "third"));
+                fourthDanDate.setText(getNonNullDate(documentSnapshot, "fourth"));
                 fifthDanDate.setText(getNonNullDate(documentSnapshot, "fifth"));
+                sixthDanDate.setText(getNonNullDate(documentSnapshot, "sixth"));
+                seventhDanDate.setText(getNonNullDate(documentSnapshot, "seventh"));
+                eighthDanDate.setText(getNonNullDate(documentSnapshot, "eighth"));
+                ninthDanDate.setText(getNonNullDate(documentSnapshot, "ninth"));
+                tenthDanDate.setText(getNonNullDate(documentSnapshot, "tenth"));
             }
         });
     }
