092a95c1b5d8af97fd5d77fa0f18612eab37d2a9
Camilla Giaccari <cami.lla@me.com>
2023-03-13 10:40:30 +0100

    The belt log now reads a specific belt from DB and shows it (#17)

--- app/src/main/java/it/units/karatepassport/BeltLogFragment.java
+++ app/src/main/java/it/units/karatepassport/BeltLogFragment.java
@@ -3,19 +3,55 @@ package it.units.karatepassport;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import javax.annotation.Nullable;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Date;
+
 public class BeltLogFragment extends Fragment {
+
+    TextView beltHere;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_belt_log, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        beltHere = getView().findViewById(R.id.write_the_belt_here);
+
+        // initialization of Auth e Firestore
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        // this is the reference to what we want to retrieve, to the specific user
+        DocumentReference documentReference = fStore.collection("users")
+                .document(userID).collection("belts").document("belts");
+
+        documentReference.addSnapshotListener((documentSnapshot, e) -> {
+            if ((e == null) || (documentSnapshot != null && documentSnapshot.exists())) {
+                Date timestamp = documentSnapshot.getTimestamp("fifth").toDate();
+                String date = DateFormat.format("MMMM dd, yyyy", timestamp).toString();
+                beltHere.setText(date);
+            }
+        });
     }
 }
